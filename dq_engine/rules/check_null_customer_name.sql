300fc102a95d42f09fbf0e13e84194ca62917140
YANqueen2023 <134961269+YANqueen2023@users.noreply.github.com>
2025-11-30 23:16:03 -0500

    Enhance NULL customer name check with percentage
    Added calculation for percentage of NULL customer names.

--- dq_engine/rules/check_null_customer_name.sql
+++ dq_engine/rules/check_null_customer_name.sql
@@ -1,7 +1,8 @@
 -- Rule: Check for NULL customer_name values
 -- Purpose: Ensure customer_name is always populated for analytics 
 
-SELECT COUNT(*) AS null_customer_name_count
+SELECT
+    COUNT(*) AS null_customer_name_count,
+    COUNT(*)*1.0/SELECT(COUNT(*) FROM customers) AS null_customer_name_percentage
 FROM customers
-WHERE customer_name IS NULL
-AND is_active = 1;    
+WHERE customer_name IS NULL;    
